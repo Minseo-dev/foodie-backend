30c9178ff6df2ad49efa4688074872b0476db50b
Minseo-dev <kms12782@gmail.com>
2021-10-02 18:13:57 +0900

    Update Class name

--- src/foodie/DBAccess.java
+++ src/foodie/DBAccess.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MemberJoinDAO {
+public class DBAccess {
   private static Connection conn = null;
 
   public void init() {
