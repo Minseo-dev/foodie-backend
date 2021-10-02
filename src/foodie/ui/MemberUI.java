c0b222378a3295ec3368a0d632957975e42daf63
Minseo-dev <kms12782@gmail.com>
2021-10-02 18:04:16 +0900

    ServiceOption을 enum 타입으로 변경

--- src/foodie/ui/MemberUI.java
+++ src/foodie/ui/MemberUI.java
@@ -1,36 +1,44 @@
 package foodie.ui;
 
-import foodie.exception.AuthenException;
-import foodie.member.Member;
+import foodie.member.MemberServiceFunction;
 
 import java.util.Scanner;
 
+enum ServiceOption {TERMINATED, LOGIN, FIND_ID, SIGNUP}
+
 public class MemberUI {
   Scanner sc = new Scanner(System.in);
-  Member member = new Member();
 
-  private int menu() {
+  MemberServiceFunction memberServiceFunction = new MemberServiceFunction();
+
+  private ServiceOption menu() {
     System.out.println("Welcome to Foodie");
-    return getNumInput("[1] 로그인 [2] 회원가입 [0] 종료");
+    int number = getNumInput("[1] 로그인 [2] 아이디 찾기 [3] 회원가입 [0] 종료");
+    return ServiceOption.values()[number];
   }
 
   private int getNumInput(String msg) {
     System.out.println(msg);
-    return sc.nextInt();
+    return sc.nextInt(); //nextInt 문제 많음
   }
 
-  public void run() throws AuthenException {
-    int key = 0;
-    while ((key = menu()) != 0) {
-      switch (key) {
-        case 1:
-          member.searchId();
+  public void run() {
+    ServiceOption serviceOption = null;
+    while (serviceOption != ServiceOption.TERMINATED) {
+      serviceOption = menu();
+      switch (serviceOption) {
+        case LOGIN:
           break;
-        case 2:
-          member.memberJoin();
+        case FIND_ID:
+          memberServiceFunction.findId();
+          break;
+        case SIGNUP:
+          memberServiceFunction.signUpMember();
+          System.out.println("회원가입이 완료되었습니다.");
           break;
       }
     }
   }
+
 
 }
