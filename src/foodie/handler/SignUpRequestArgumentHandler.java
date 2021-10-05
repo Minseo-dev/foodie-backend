3372d65f3b220813e41226d47c4750f788b07f9c
Minseo-dev <kms12782@gmail.com>
2021-10-05 19:09:48 +0900

    이메일 조건문 합치기

--- src/foodie/handler/SignUpRequestArgumentHandler.java
+++ src/foodie/handler/SignUpRequestArgumentHandler.java
@@ -2,14 +2,14 @@ package foodie.handler;
 
 
 import foodie.exception.AuthenException;
-import foodie.service.MemberDAO;
+import foodie.dao.MemberDAO;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SignUpRequestArgumentHandler extends Throwable {
 
- public static final Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+  public static final Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
 
   MemberDAO memberDAO = new MemberDAO();
 
@@ -40,7 +40,7 @@ public class SignUpRequestArgumentHandler extends Throwable {
       throw new AuthenException("아이디는 영문자와 숫자를 혼용해서 만들어주세요.");
     }
 
-    if (memberDAO.checkID(id) == true) {
+    if (memberDAO.duplicateCheckID(id)) {
       throw new AuthenException("중복된 아이디입니다.");
     }
 
@@ -74,24 +74,22 @@ public class SignUpRequestArgumentHandler extends Throwable {
 
   public void nickNameStandard(String nickName) throws AuthenException {
 
-    if (nickName.length() < 0 || nickName.length() > 20) {
+    if (nickName.length() > 20) {
       throw new AuthenException("3자~18자 이내의 별명만 가능합니다.");
     }
   }
 
-  public static boolean validate(String email){
+  public static boolean validate(String email) {
     Matcher matcher = EMAIL_REGEX.matcher(email);
     return matcher.find();
   }
+
   public void emailStandard(String email) throws AuthenException {
-   
+
     String[] strings = email.split(" ");
-    
-    if (strings.length > 1) {
+
+    if (strings.length > 1 || !validate(email)) {
       throw new AuthenException("이메일을 잘못 입력하셨습니다.");
-    }
-    if (!validate(email)) {
-      throw new AuthenException("이메일 유효 없음.");
     }
 
 
