ea06b39b286acbfded62de9d796ed75e58cba249
Minseo-dev <kms12782@gmail.com>
2021-10-07 03:28:10 +0900

    Implement sigleton scanner

---
+++ src/foodie/Input.java
@@ -0,0 +1,34 @@
+package foodie;
+
+import java.util.Scanner;
+
+public class Input {
+  private static Input instance = null;
+
+  private Scanner scanner;
+
+  private Input() {
+    scanner = new Scanner(System.in);
+  }
+
+  public static Input getInstance() {
+    if (instance == null) {
+      instance = new Input();
+    }
+    return instance;
+  }
+
+  public int getInt() {
+    while (!scanner.hasNextInt()) {
+      System.out.println("숫자를 입력해주세요");
+    }
+    return scanner.nextInt();
+  }
+
+  public String getString() {
+    String sInput = scanner.nextLine();
+    scanner.reset();
+    return sInput;
+  }
+
+}
